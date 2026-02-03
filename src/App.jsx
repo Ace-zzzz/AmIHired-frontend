9ff483ed27004c803ccd657fd47fcf7c5d8dfdb6
Ace-zzzz <escobia357@gmail.com>
2026-02-04 01:34:03 +0800

    feat: Add Security layer for protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ModalProvider from './provider/ModalProvider';
+import ProtectedRoute from './auth/ProtectedRoute';
 
 function App() {
   return (
@@ -11,7 +12,11 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
         <Route path='/login' element={<Login/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
+
+        {/*PROTECTED ROUTES*/}
+        <Route element={ <ProtectedRoute/> }>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+        </Route>
       </Routes>
 
       <ModalProvider />
---
+++ src/auth/ProtectedRoute.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoute = () => {
+    // GET TOKEN
+    const token = localStorage.getItem("token");
+
+    /**
+     * REDIRECT TO 
+     * LOGIN PAGE IF
+     * NO TOKEN STORED 
+     **/
+    if (!token) {
+        return <Navigate to={"/login"} replace/>
+    }
+
+    return <Outlet/>
+}
+
+export  default ProtectedRoute;
